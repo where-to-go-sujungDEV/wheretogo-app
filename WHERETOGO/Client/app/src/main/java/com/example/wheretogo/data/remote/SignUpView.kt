bbf92392385afce07a2caee84a8fe569d1082780
hyun2371 <loveanna1228@gmail.com>
2022-07-31 18:08:39 +0900

    Fix: revise directory

--- WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/SignUpView.kt
+++
@@ -1,6 +0,0 @@
-package com.example.wheretogo.data.remote
-
-interface SignUpView { //액티비티와 AuthService 연결
-    fun onSignUpSuccess()
-    fun onSignUpFailure()
-}
--- WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/loginView.kt
+++
@@ -1,7 +0,0 @@
-package com.example.wheretogo.data.remote
-
-interface LoginView {
-    fun onLoginSuccess(code : Int, result: Result)
-    fun onLoginFailure(code: Int, result: Result)
-
-}
