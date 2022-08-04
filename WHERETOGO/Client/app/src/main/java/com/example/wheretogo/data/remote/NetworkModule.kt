faa1c8b253476428fad610752c91cc5281020844
hyun2371 <loveanna1228@gmail.com>
2022-08-04 21:12:13 +0900

    Feat: Edit AuthService

--- WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/NetworkModule.kt
+++ WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/NetworkModule.kt
@@ -3,10 +3,12 @@ package com.example.wheretogo.data.remote
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "https://edu-api-test.softsquared.com"
+
+const val BASE_URL = "http://10.0.2.2:3000"
 fun getRetrofit(): Retrofit {
     val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create()).build()
 
     return retrofit
 }
+
