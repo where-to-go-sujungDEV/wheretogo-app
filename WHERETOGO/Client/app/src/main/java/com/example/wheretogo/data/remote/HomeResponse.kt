5063978b30efea978c7f552c08342729695d03c1
hyun2371 <loveanna1228@gmail.com>
2022-08-12 21:50:52 +0900

    Feat: Add popular event response

--- WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/HomeResponse.kt
+++ WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/HomeResponse.kt
@@ -7,11 +7,30 @@ data class MainEventResponse(
     @SerializedName(value = "code")val code:Int,
     @SerializedName(value = "isSuccess")val isSuccess:Boolean,
     @SerializedName(value = "results")val results:ArrayList<MainEventResult>,
-
 )
 
 data class MainEventResult(
-    @SerializedName(value = "code")val mainEventID:Int,
+    @SerializedName(value = "mainEventID")val mainEventID:Int,
     @SerializedName(value = "ment")val ment: String,
     @SerializedName(value = "Img")val Img: String
+)
+
+//홈화면 인기 배너
+data class PopularEventResponse(
+    @SerializedName(value = "code")val code:Int,
+    @SerializedName(value = "isSuccess")val isSuccess:Boolean,
+    @SerializedName(value = "results")val results:ArrayList<PopularEventResult>,
+)
+
+data class PopularEventResult(
+    @SerializedName(value = "eventID")val eventID:Int,
+    @SerializedName(value = "eventName")val eventName:String,
+    @SerializedName(value = "startDate")val startDate:String,
+    @SerializedName(value = "endDate")val endDate:String,
+    @SerializedName(value = "savedNum")val savedNum:Int,
+    @SerializedName(value = "Img")val Img:String,
+    @SerializedName(value = "hashtag1")val hashtag1:String,
+    @SerializedName(value = "hashtag2")val hashtag2:String,
+    @SerializedName(value = "hashtag3")val hashtag3:String,
+
 )
