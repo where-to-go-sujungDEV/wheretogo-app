c5292e426709f62d9c0fb835aa94be4722f83f75
waterjin <waterjin326@gmail.com>
2022-08-13 16:18:46 +0900

    add mainEventTBL events

--- WHERETOGO/Server/models/event.js
+++ WHERETOGO/Server/models/event.js
@@ -1,7 +1,7 @@
 import db from "../config/dbConnection.js";
 
 export const getMainBoardContents = (result) => {
-    db.query("Select mainEventID, ment, CONCAT('http://localhost:3000/asset/mainEvent/', prePic, '.jpg') as Img from mainEventTBL;", (err, results) => {             
+    db.query("Select mainEventID, ment, CONCAT('http://localhost:3000/asset/mainEvent/', prePic, '.jpg') as Img, event1ID, event2ID, event3ID, event4ID, event5ID from mainEventTBL;", (err, results) => {             
         if(err) {
             console.log(err);
             result(err, null);
@@ -15,7 +15,7 @@ export const getMainBoardContents = (result) => {
 }
 
 export const getTopContents = (result) => { 
-    db.query("select eventID,eventName,startDate, endDate, savedNum, CONCAT('http://localhost:3000/asset/event/', pic, '.jpg') as Img, hashtag1, hashtag2, hashtag3 from eventTBL ORDER BY savedNum DESC LIMIT 5;", (err, results) => {             
+    db.query("select eventID,eventName,startDate, endDate, savedNum, CONCAT('http://localhost:3000/asset/event/', pic, '.jpg') as Img, genre, kind, theme from eventTBL ORDER BY savedNum DESC LIMIT 5;", (err, results) => {             
         if(err) {
             console.log(err);
             result(err, null);
