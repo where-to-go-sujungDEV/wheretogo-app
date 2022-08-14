989f401c3add173be40397c0830c8d13edd3a772
waterjin <waterjin326@gmail.com>
2022-08-14 17:25:08 +0900

    add search API

--- WHERETOGO/Server/controllers/searchController.js
+++ WHERETOGO/Server/controllers/searchController.js
@@ -1,16 +1,6 @@
 import {getSearchResults} from "../models/search.js";
 
 export const getSearch = (req, res) => {
-    getSearchResults((err, results) => {
-        if (err){
-            res.send(err);
-        }else{
-            res.json(results);
-        }
-    });
-}
-/*
-export const getSearch = (req, res) => {
     const data = req.body;
     getSearchResults(data, (err, results) => {
         if (err){
@@ -20,4 +10,3 @@ export const getSearch = (req, res) => {
         }
     });
 }
-*/
--- WHERETOGO/Server/models/search.js
+++ WHERETOGO/Server/models/search.js
@@ -1,10 +1,87 @@
 import db from "../config/dbConnection.js";
 
-export const getSearchResults = (result) => {
-    db.query("select * from eventTBL;", (err, results) => {             
+export const getSearchResults = (data, result) => {
+    var qr = 'select eventID, eventName, genre, kind, theme, startDate, endDate, pic, w1+w2+w3+w4+w6+m1+m2+m3+m4+m6 as savedNum from eventTBL ';
+    qr += ' where ';
+
+    if((!data.fromD)&&(!data.toD)) {
+        qr += ' endDate >= now() ';
+    }
+    else if((!data.fromD)&&(data.toD)){
+        qr += ' (endDate >= now() and startDate <= \'';
+        qr += data.toD;
+        qr += '\') ';
+    }
+    else if((data.fromD)&&(!data.toD)){
+        qr += ' (endDate >= \'';
+        qr += data.fromD;
+        qr += '\') ';
+    }
+    else if((data.fromD)&&(data.toD)){
+        qr += ' (endDate >= \'';
+        qr += data.fromD;
+        qr += '\' and startDate <= \'';
+        qr += data.toD;
+        qr += '\') ';
+    }
+
+    if(data.search) {
+        qr += ' and (eventName like \'\%';
+        qr += data.search;
+        qr += '\%\' or content like \'\%'
+        qr += data.search;
+        qr += '\%\') ';
+    }
+
+    if(data.genre){
+        qr += ' and genre in (';
+        qr += data.genre;
+        qr += ') ';
+    }
+
+    if(data.kind){
+        qr += ' and kind in (';
+        qr += data.kind;
+        qr += ') ';
+    }
+
+    if(data.theme){
+        qr += ' and theme in (';
+        qr += data.theme;
+        qr += ') ';
+    }
+
+    if(data.dou){
+        qr += ' and dou = \'';
+        qr += data.dou;
+        qr += '\' ';
+    }
+
+    if(data.si){
+        qr += ' and si = \'';
+        qr += data.si;
+        qr += '\' ';
+    }
+
+    qr += ' ORDER BY ';
+
+    if(data.align == 'start'){
+        qr += ' startDate ASC;';
+    }
+    else if(data.align == 'end'){
+        qr += ' endDate ASC;';
+    }
+    else {
+        qr+= ' savedNum DESC;';
+    }
+
+    db.query(qr, (err, results) => {             
         if(err) {
             console.log(err);
-            result(err, null);
+            result({
+                code : 500,
+                isSuccess : false,
+                err}, null);
         } else {
             result(null, {
                 code : 200,
@@ -13,15 +90,3 @@ export const getSearchResults = (result) => {
         }
     }); 
 }
-
-
-/*export const getSearchResults = (data, result) => {
-    db.query("select * from eventTBL where si = ? and dou = ?;", [data.si, data.dou], (err, results) => {             
-        if(err) {
-            console.log(err);
-            result(err, null);
-        } else {
-            result(null, results);
-        }
-    });   
-}*/
