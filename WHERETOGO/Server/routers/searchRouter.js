55bb7accdc3f239f57dbe97a928d5c153a9e63fc
waterjin <waterjin326@gmail.com>
2022-08-15 01:03:52 +0900

    change getSearch API into POST

--- WHERETOGO/Server/routers/searchRouter.js
+++ WHERETOGO/Server/routers/searchRouter.js
@@ -8,7 +8,7 @@ import {getSearch, getHotSearch, updateSearch} from "../controllers/searchContro
 const searchRouter = express.Router();
   
 
-searchRouter.get('/', getSearch);
+searchRouter.post('/', getSearch);
 
 searchRouter.get('/hot', getHotSearch);
 
