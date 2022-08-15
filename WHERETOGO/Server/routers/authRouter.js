ec473f2f0d179dd5220403b92f783a538bedaa17
waterjin <waterjin326@gmail.com>
2022-08-15 23:20:25 +0900

    fix authRouter

--- WHERETOGO/Server/routers/authRouter.js
+++ WHERETOGO/Server/routers/authRouter.js
@@ -2,7 +2,6 @@ import express from 'express';
 const router = express.Router();
 import db from '../config/dbConnection.js';
 
-//import { hashSync, genSaltSync, compareSync } from "bcrypt";
 import bcrypt from 'bcryptjs';
 import { check, validationResult } from 'express-validator';
 
@@ -16,7 +15,14 @@ router.post('/sign-up', [
   db.query(
     `SELECT * FROM userTBL WHERE LOWER(email) = LOWER(${db.escape(req.body.email)});`,
       (err, result) => {
-        if (result.length > 0) {
+        if(err){
+          return res.status(500).send({
+            msg : "오류가 발생하였습니다.",
+            code : 500, 
+            isSuccess : false,
+            err});
+        }
+        else if (result.length > 0) {
           return res.status(409).send({
             msg: '이미 등록된 유저입니다',
             code : 409,
@@ -39,17 +45,19 @@ router.post('/sign-up', [
                   req.body.email)}, ${db.escape(hash)},'${req.body.nickName}', ?, ?);`, [req.body.sex, req.body.age] ,
                   (err, result) => {
                     if (err) {
-                      throw err;
-                      return res.status(400).send({
-                        msg: err
+                      return res.status(500).send({
+                        msg: '회원가입에 실패하였습니다',
+                        code : 500,
+                        isSuccess : false,
+                        err
                       });
                     }
-                    return res.status(201).send({
-                      msg: '회원가입에 성공하였습니다',
-                      code : 201,
-                      isSuccess : true
-
-                    });
+                    else {
+                      return res.status(201).send({
+                        msg: '회원가입에 성공하였습니다',
+                        code : 201,
+                        isSuccess : true});
+                    }
                   }
                   );}
         });
@@ -66,53 +74,54 @@ router.post('/login',  [
     (err, result) => {
       // user does not exists
       if (err) {
-        throw err;
-        return res.status(400).send({
-          msg: err
+        return res.status(500).send({
+          msg : "오류가 발생하였습니다.",
+          code : 500, 
+          isSuccess : false,
+          err
         });
       }
-      if (!result.length) {
+      else if (!result.length) {
         return res.status(401).send({
-          msg: '이메일이나 비밀번호가 올바르지 않습니다',
+          msg: '이메일이 올바르지 않거나, 등록되지않은 유저입니다.',
           code : 401,
           isSuccess : false
         });
       }
-      // check password
-      bcrypt.compare(
-        req.body.password,
-        result[0]['pw'],
-        (bErr, bResult) => {
-          // wrong password
+      else {
+        // check password
+        bcrypt.compare (req.body.password,result[0]['pw'], (bErr, bResult) => {
+
           if (bErr) {
+            return res.status(500).send({
+              msg : "오류가 발생하였습니다.",
+              code : 500, 
+              isSuccess : false,
+              err
+            });
+          }
+          else if(!bResult){
             return res.status(401).send({
-              msg: '이메일이나 비밀번호가 올바르지 않습니다',
+              msg: '비밀번호가 틀렸습니다.',
               code : 401,
               isSuccess : false
             });
           }
-          if (bResult) {
+          else {
             const token = jwt.sign({id:result[0].id},'the-super-strong-secrect',{ expiresIn: '1h' });
-            db.query(
-              `UPDATE userTBL SET last_login = now() WHERE email = '${result[0].email}'`
-              );
+            db.query(`UPDATE userTBL SET last_login = now() WHERE email = '${result[0].email}'`);
               return res.status(200).send({
                 msg: '로그인에 성공하였습니다',
                 code : 200,
                 isSuccess : true,
-                token,
+                token : token,
                 user: result[0]
               });
             }
-            return res.status(401).send({
-              msg: '이메일이나 비밀번호가 올바르지 않습니다',
-              code : 401,
-              isSuccess : false
-            });
           }
           );
-        }
-        );
+      }}
+  );
 });
 
 router.post('/auto-login',  [
@@ -136,9 +145,9 @@ router.post('/auto-login',  [
     const decoded = jwt.verify(theToken, 'the-super-strong-secrect');
     db.query(
       `SELECT * FROM userTBL where email='${decoded.email}'`,function (error, results, fields) {
-    if (error) throw error;
-    return res.send({ isSuccess: true, data: results[0], message: '사용자 로그인 정보 확인되었습니다.' });
-    });
+    if ((error)||(!results)) res.send({isSuccess: false, msg : '자동로그인 실패하였습니다.', code : 500});
+    else {return res.send({ isSuccess: true, data: results[0], msg: '사용자 로그인 정보 확인되었습니다.', code : 200 });
+}});
   });
 
 export default router;
