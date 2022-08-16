1d22caef8a695d0154999d1a5029d04c86da889c
hyun2371 <loveanna1228@gmail.com>
2022-08-16 22:36:42 +0900

    Feat: Edit authservice

--- WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/auth/AuthService.kt
+++ WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/auth/AuthService.kt
@@ -22,9 +22,9 @@ class AuthService { //signupview 변수 받음
     fun signUp(signUpInfo: SignUpInfo){
         authService.signUp(signUpInfo).enqueue(object: Callback<SignUpResponse> {
             override fun onResponse(call: Call<SignUpResponse>, response: Response<SignUpResponse>) {
-                Log.d("login/Response/response.code",response.code().toString())
+                Log.d("sign/Response/response.code",response.code().toString())
                 val resp: SignUpResponse = response.body()!!
-                Log.d("login/Response/resp.code",resp.code.toString())
+                Log.d("sign/Response/resp.code",resp.code.toString())
                 when(resp.code){
                     201 ->signUpView.onSignUpSuccess(resp.msg)
                     409,500 ->{
