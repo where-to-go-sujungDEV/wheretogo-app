26bc987c4d957caeecb0db4bccdb357cee1d9da3
hyun2371 <loveanna1228@gmail.com>
2022-08-21 20:09:41 +0900

    Feat: 회원탈퇴 구현

--- WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/auth/AuthRetrofitInterface.kt
+++ WHERETOGO/Client/app/src/main/java/com/example/wheretogo/data/remote/auth/AuthRetrofitInterface.kt
@@ -1,8 +1,11 @@
 package com.example.wheretogo.data.remote.auth
 
+import androidx.room.Delete
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface AuthRetrofitInterface {
     @POST("/user/sign-up")
@@ -10,6 +13,11 @@ interface AuthRetrofitInterface {
 
     @POST("/user/login")
     fun login(@Body appLoginInfo: LoginInfo): Call<LoginResponse>
+
+    @DELETE("/user/unregister/{userID}")
+    fun deleteUser(@Path("userID") userID: Int): Call<DeleteUserResponse>
+
+
 }
 
 
--- WHERETOGO/Client/app/src/main/java/com/example/wheretogo/ui/setting/SettingActivity.kt
+++ WHERETOGO/Client/app/src/main/java/com/example/wheretogo/ui/setting/SettingActivity.kt
@@ -1,12 +1,27 @@
 package com.example.wheretogo.ui.setting
 
 import android.content.Intent
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wheretogo.ApplicationClass
+import com.example.wheretogo.data.remote.auth.AuthRetrofitInterface
+import com.example.wheretogo.data.remote.auth.DeleteUserResponse
+import com.example.wheretogo.data.remote.auth.getRetrofit
 import com.example.wheretogo.databinding.ActivitySettingBinding
 import com.example.wheretogo.ui.BaseActivity
+import com.example.wheretogo.ui.MainActivity
 import com.example.wheretogo.ui.keyword.KeywordActivity
+import com.example.wheretogo.ui.splash.SplashActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SettingActivity: BaseActivity<ActivitySettingBinding>(ActivitySettingBinding::inflate) {
+    private val service = getRetrofit().create(AuthRetrofitInterface::class.java)
+
     override fun initAfterBinding() {
+
         binding.settingChangeNickname.setOnClickListener {
             startNextActivity(ChangeInfoActivity::class.java)
         }
@@ -26,5 +41,57 @@ class SettingActivity: BaseActivity<ActivitySettingBinding>(ActivitySettingBindi
             startNextActivity(KeywordActivity::class.java)
         }
 
+        leavePanelClickListener()
+
+
+    }
+    private fun leavePanelClickListener() {
+        //탈퇴하기
+        binding.settingResignTv.setOnClickListener {
+            binding.leaveConfirmPanel.visibility = View.VISIBLE
+        }
+        //탈퇴 확인
+        binding.leaveOkayBtn.setOnClickListener {
+            deleteUser()
+        }
+        //탈퇴 취소
+        binding.leaveCancelBtn.setOnClickListener {
+            binding.leaveConfirmPanel.visibility = View.INVISIBLE
+        }
+        //탈퇴 완료
+        binding.leaveFinishBtn.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun deleteUser(){
+        Log.d("deleteUser","함수 실행")
+        service.deleteUser(getIdx()).enqueue(object: Callback<DeleteUserResponse> {
+            override fun onResponse(call: Call<DeleteUserResponse>, response: Response<DeleteUserResponse>) {
+                val resp = response.body()!!
+                Log.d("deleteUser",resp.code.toString())
+                when (resp.code){
+                    200->{
+                        binding.leaveConfirmPanel.visibility= View.INVISIBLE
+                        binding.leaveFinishPanel.visibility= View.VISIBLE
+
+                        val spf = getSharedPreferences("userInfo",MODE_PRIVATE)
+                        val editor = spf!!.edit()
+                        editor.remove("userIdx") //키값에 저장된값 삭제-> idx=-1
+                        editor.apply()
+                    }
+                    204->showToast(resp.msg)
+
+                }
+            }
+            override fun onFailure(call: Call<DeleteUserResponse>, t: Throwable) {
+            }
+        })
+    }
+
+    //유저 인덱스 가져옴
+    private fun getIdx(): Int {
+        val spf = getSharedPreferences("userInfo", MODE_PRIVATE)
+        return spf!!.getInt("userIdx",-1)
     }
 }
