464e563fb4d2bca4b64be63e8aa03e8ba83ac461
waterjin <waterjin326@gmail.com>
2022-09-10 20:25:03 +0900

    fix dbConfig

--- WHERETOGO/Server/config/dbConnection.js
+++ WHERETOGO/Server/config/dbConnection.js
@@ -5,13 +5,6 @@ const db = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "vmfhwprxm3!",
-<<<<<<< HEAD
-  password: "MySQLsujin!",
-=======
-  password: "vmfhwprxm3!",
->>>>>>> 20d82934b1debc8f1fd9466dde40a528f2fc64c2
-
-database: "where2goDB",
   database: "where2goDB",
 });
 
