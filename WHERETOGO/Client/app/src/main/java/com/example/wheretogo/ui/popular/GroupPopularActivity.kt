11234c8527f6d6bc2b45897e77269b5991a87fc5
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-03-30 22:15:24 +0900

    [Feat] 그룹 인기 이벤트 액티비티 추가

---
+++ WHERETOGO/Client/app/src/main/java/com/example/wheretogo/ui/popular/GroupPopularActivity.kt
@@ -0,0 +1,10 @@
+package com.example.wheretogo.ui.popular
+
+import com.example.wheretogo.databinding.ActivityGroupPopularBinding
+import com.example.wheretogo.ui.BaseActivity
+
+class GroupPopularActivity: BaseActivity<ActivityGroupPopularBinding>(ActivityGroupPopularBinding::inflate) {
+    override fun initAfterBinding() {
+        TODO("Not yet implemented")
+    }
+}
