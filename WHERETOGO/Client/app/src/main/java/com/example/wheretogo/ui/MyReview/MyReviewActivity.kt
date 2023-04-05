1c5b32911aae38ff1be97d0e1eb89cf93ee504ee
hyeon-b <20201009@sungshin.ac.kr>
2023-04-05 15:08:06 +0900

    style:내가 쓴 리뷰 레이아웃명 수정

--- WHERETOGO/Client/app/src/main/java/com/example/wheretogo/ui/MyReview/MyReviewActivity.kt
+++ WHERETOGO/Client/app/src/main/java/com/example/wheretogo/ui/MyReview/MyReviewActivity.kt
@@ -1,12 +1,12 @@
-package com.example.wheretogo.ui.visited
+package com.example.wheretogo.ui.MyReview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.wheretogo.R
 
-class VisitedActivity : AppCompatActivity() {
+class MyReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_visited)
+        setContentView(R.layout.activity_myreview)
     }
 }
