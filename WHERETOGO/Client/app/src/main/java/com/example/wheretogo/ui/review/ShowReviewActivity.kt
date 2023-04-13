629e872a6258535c4f603c0a62e12db8d608e324
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-04-13 23:07:15 +0900

    [Feat] 전체 리뷰 보기 액티비티 스피너 추가

---
+++ WHERETOGO/Client/app/src/main/java/com/example/wheretogo/ui/review/ShowReviewActivity.kt
@@ -0,0 +1,37 @@
+package com.example.wheretogo.ui.review
+
+import android.graphics.Color
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import com.example.wheretogo.R
+import com.example.wheretogo.databinding.ActivityShowReviewBinding
+import com.example.wheretogo.ui.BaseActivity
+
+class ShowReviewActivity: BaseActivity<ActivityShowReviewBinding>(ActivityShowReviewBinding::inflate) {
+    private val gender = arrayOf("별점 높은 순","별점 낮은 순","추천순")
+    override fun initAfterBinding() {
+        initSpinner()
+        binding.showReviewBackIv.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun initSpinner(){
+        val spinner = findViewById<Spinner>(R.id.show_review_sort_spinner)
+        val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,gender)
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if (p0 != null) {
+                    (p0.getChildAt(0) as TextView).setTextColor(Color.parseColor("#4C00C4"))
+                }
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+
+    }
+}
