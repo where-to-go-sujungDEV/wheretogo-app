fd2c59f2db4b28b27d62ce4378a1c00f75f32d20
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-05-24 22:13:03 +0900

    [Feat] 수평 그래프 연습2

--- WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/ui/detail/PracticeActivity.kt
+++ WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/ui/detail/PracticeActivity.kt
@@ -2,7 +2,6 @@ package com.sjdev.wheretogo.ui.detail
 
 import android.graphics.Color
 import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.BarData
@@ -10,6 +9,7 @@ import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 
 import com.sjdev.wheretogo.databinding.ActivityPracticeBinding
+
 import com.sjdev.wheretogo.ui.BaseActivity
 
 
@@ -21,9 +21,7 @@ class PracticeActivity : BaseActivity<ActivityPracticeBinding>(ActivityPracticeB
     override fun initAfterBinding() {
         var barChart : BarChart = binding.barChart // 차트 선언
 
-        barChart.setTouchEnabled(false) // 터치 금지
-        barChart.setDrawBarShadow(true) // 그래프 그림자
-        barChart.description.isEnabled = false // description label 비활성화
+
         //값 추가
         barList = ArrayList()
         barList.add(BarEntry(1f, 50.8f))
@@ -39,38 +37,43 @@ class PracticeActivity : BaseActivity<ActivityPracticeBinding>(ActivityPracticeB
         barData = BarData(barDataSet)
         barDataSet.color = Color.parseColor("#4C00C4") // 그래프 색상 지정
         barChart.data = barData
-
         // 글자 속성
         barDataSet.valueTextColor = Color.BLACK
         barDataSet.valueTextSize = 10f
+        barData.barWidth = 0.7f
+        barChart.run {
+
+            setTouchEnabled(false) // 터치 금지
+            setDrawBarShadow(true) // 그래프 그림자
+            description.isEnabled = false // description label 비활성화
+            legend.isEnabled = false // 범례 비활성화
 
 
-        val xAxis : XAxis = barChart.xAxis
+            xAxis.run {
+                isEnabled = false // x축 값 표시
+                position = XAxis.XAxisPosition.BOTTOM
+                setDrawGridLines(false) // 격자구조
+                setDrawAxisLine(false) // 그림
 
-        xAxis.run{
-            isEnabled = true // x축 값 표시
-            position = XAxis.XAxisPosition.BOTTOM
-            setDrawGridLines(false) // 격자구조
-            granularity = 1f // x축 간격
-            setDrawAxisLine(false) // 그림
+            }
+
+            axisLeft.run { // 왼쪽 y축
+                isEnabled = false
+                setDrawAxisLine(false)
+                setDrawGridLines(false)
+                axisMinimum = 0f // this replaces setStartAtZero(true)
+                axisMaximum = 100f
+            }
+
+            axisRight.run { //오른쪽 y축
+                isEnabled = false // y축 오른쪽 값 표시
+                setDrawAxisLine(false)
+                setDrawGridLines(false)
+                axisMinimum = 0f
+                axisMaximum = 100f
+            }
+
         }
-
-        val yl: YAxis = barChart.axisLeft
-        yl.run{
-            setDrawAxisLine(false)
-            setDrawGridLines(false)
-            axisMinimum = 0f // this replaces setStartAtZero(true)
-        }
-
-        val yr: YAxis = barChart.axisRight
-        yr.run{
-            setDrawAxisLine(false)
-            setDrawGridLines(false)
-            axisMinimum = 0f
-        }
-
-//        barChart.setFitBars(true)
-
 
     }
 
