18bbf7700a4c6a99d6164d1a92eb089bd2a98bcb
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-05-25 19:46:26 +0900

    [Feat] 그래프 라벨, %, 수치 잘림 해결

--- WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/ui/detail/PracticeActivity.kt
+++ WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/ui/detail/PracticeActivity.kt
@@ -7,9 +7,8 @@ import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
-
+import com.github.mikephil.charting.formatter.PercentFormatter
 import com.sjdev.wheretogo.databinding.ActivityPracticeBinding
-
 import com.sjdev.wheretogo.ui.BaseActivity
 import java.util.*
 
@@ -23,70 +22,75 @@ class PracticeActivity : BaseActivity<ActivityPracticeBinding>(ActivityPracticeB
         // 값 추가
         val values = mutableListOf<BarEntry>()
         val type = ArrayList<String>()
+        val percent = ArrayList<Float>()
+
         type.add("친구와")
         type.add("연인과")
         type.add("동생")
         type.add("친구와")
         type.add("동생")
         type.add("동생")
-        type.add("친구와")
-        for (i in 0 .. 6){
+        type.add("친친구와")
+
+        percent.add(100f)
+        percent.add(40f)
+        percent.add(30.5f)
+        percent.add(20f)
+        percent.add(10f)
+        percent.add(20f)
+        percent.add(20f)
+
+
+        for (i in 0..6) {
             val index = i.toFloat()
-            val random = Random().nextInt(101).toFloat()
-            values.add(BarEntry(index, random))
+            values.add(BarEntry(index, percent[i]))
         }
 
         val set = BarDataSet(values, "방문 비율")         // 차트 데이터 리스트 삽입
-            .apply{
+            .apply {
                 color = Color.parseColor("#4C00C4") // 그래프 색상 지정
                 setDrawValues(true) // 그래프 값 표시
-                valueFormatter = IndexAxisValueFormatter()
                 valueTextColor = Color.BLACK
                 valueTextSize = 10f
             }
-
-        val barChart : BarChart = binding.barChart // 차트 선언
+        val barChart: BarChart = binding.barChart // 차트 선언
         barData = BarData(set)
-            .apply{
-                barWidth = 0.7f
+            .apply {
+                barWidth = 0.6f // 막대 너비
+                setValueFormatter(PercentFormatter()) // '%' 붙이기
             }
-        barData = BarData(set)
 
         barChart.data = barData
 
         barChart.run {
-
             setTouchEnabled(false) // 터치 금지
             setDrawBarShadow(true) // 그래프 그림자
 //            setVisibleXRange(1f,14f) // x좌표 기준 보여줄 그래프 개수 조정
 
             description.isEnabled = false // description label 비활성화
             legend.isEnabled = false // 범례 비활성화
+            extraRightOffset = 40f; // 수치값 잘리지 않도록 오른쪽에 공간 부여
 
-
-            xAxis.run {
+            xAxis.run { // x 축
                 isEnabled = true // x축 값 표시
-                valueFormatter = IndexAxisValueFormatter(type)
-                position = XAxis.XAxisPosition.BOTTOM
+                valueFormatter = IndexAxisValueFormatter(type) // 동행자 라벨 붙이기
+                textColor = Color.parseColor("#4C00C4") //라벨 색깔
+                position = XAxis.XAxisPosition.BOTTOM // 라벨 위치
                 setDrawGridLines(false) // 격자구조
-                setDrawAxisLine(false) // 그림
+                setDrawAxisLine(false)
 
             }
 
             axisLeft.run { // 왼쪽 y축
                 isEnabled = false
-                setDrawAxisLine(false)
-                setDrawGridLines(false)
                 axisMinimum = 0f // this replaces setStartAtZero(true)
                 axisMaximum = 100f
+
             }
             axisRight.run { //오른쪽 y축
                 isEnabled = false // y축 오른쪽 값 표시
-                setDrawAxisLine(false)
-                setDrawGridLines(false)
                 axisMinimum = 0f
                 axisMaximum = 100f
-
             }
 
         }
