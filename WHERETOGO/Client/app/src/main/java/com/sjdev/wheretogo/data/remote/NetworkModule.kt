54a8c3dafadae65355903b212bcea91397f01b44
hs12 <loveanna1228@gmail.com>
2023-09-09 15:01:14 +0900

    [Feat] NetworkModule 추가

--- WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/data/remote/NetworkModule.kt
+++ WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/data/remote/NetworkModule.kt
@@ -1,23 +1,36 @@
 package com.sjdev.wheretogo.data.remote
 
+import com.sjdev.wheretogo.config.XAccessTokenInterceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
-
-//const val BASE_URL = "http://52.79.207.140:3000"
-const val BASE_URL = "http://10.0.2.2:3000"
+const val X_ACCESS_TOKEN: String = "X-ACCESS-TOKEN"
+const val BASE_URL = "http://4.246.197.20:3000"
 const val KAKAO_WEB_URL = "https://dapi.kakao.com"
+const val LOCAL_URL: String = "http://10.0.2.2:3000"
+
 fun getRetrofit(): Retrofit {
-    val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create()).build()
+    val client: OkHttpClient = OkHttpClient.Builder()
+        .readTimeout(40000, TimeUnit.MILLISECONDS)
+        .connectTimeout(40000, TimeUnit.MILLISECONDS)
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     return retrofit
 }
 
 fun getKakaoRetrofit(): Retrofit {
-    val kakaoRetrofit = Retrofit.Builder().baseUrl(KAKAO_WEB_URL)
-        .addConverterFactory(GsonConverterFactory.create()).build()
+    val kakaoRetrofit = Retrofit.Builder()
+        .baseUrl(KAKAO_WEB_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     return kakaoRetrofit
 }
-
