444e235c76633e4e5b4d21a2e61df5628e0b5483
hyeon-b <20201009@sungshin.ac.kr>
2023-09-18 19:30:43 +0900

    do comment process for kakao api in MyReviewRVAdapter

--- WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/ui/myReview/MyReviewRVAdapter.kt
+++ WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/ui/myReview/MyReviewRVAdapter.kt
@@ -11,11 +11,11 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.kakao.sdk.common.util.KakaoCustomTabsClient
-import com.kakao.sdk.share.ShareClient
-import com.kakao.sdk.share.WebSharerClient
-import com.kakao.sdk.template.model.*
-import com.sjdev.wheretogo.ApplicationClass.Companion.TAG
+//import com.kakao.sdk.common.util.KakaoCustomTabsClient
+//import com.kakao.sdk.share.ShareClient
+//import com.kakao.sdk.share.WebSharerClient
+//import com.kakao.sdk.template.model.*
+//import com.sjdev.wheretogo.ApplicationClass.Companion.TAG
 import com.sjdev.wheretogo.R
 import com.sjdev.wheretogo.data.remote.myreview.MyReviewResult
 import com.sjdev.wheretogo.databinding.ItemMyreviewBinding
@@ -26,7 +26,7 @@ class MyReviewRVAdapter(private val myReviewList: ArrayList<MyReviewResult>): Re
     private lateinit var context: Context
     private val imageList = arrayListOf<String>()
 
-    lateinit var defaultFeed : FeedTemplate
+//    lateinit var defaultFeed : FeedTemplate
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyReviewRVAdapter.ViewHolder {
         context= parent.context
@@ -64,45 +64,46 @@ class MyReviewRVAdapter(private val myReviewList: ArrayList<MyReviewResult>): Re
                     }
 
                     2->{
-                        // 카카오톡 설치여부 확인
-                        if (ShareClient.instance.isKakaoTalkSharingAvailable(context)) {
-                            // 카카오톡으로 카카오톡 공유 가능
-                            ShareClient.instance.shareDefault(context, defaultFeed) { sharingResult, error ->
-                                if (error != null) {
-                                    Log.e(TAG, "카카오톡 공유 실패", error)
-                                }
-                                else if (sharingResult != null) {
-                                    Log.d(TAG, "카카오톡 공유 성공 ${sharingResult.intent}")
-                                    context.startActivity(sharingResult.intent)
-
-                                    // 카카오톡 공유에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
-                                    Log.w(TAG, "Warning Msg: ${sharingResult.warningMsg}")
-                                    Log.w(TAG, "Argument Msg: ${sharingResult.argumentMsg}")
-                                }
-                            }
-                        } else {
-                            // 카카오톡 미설치: 웹 공유 사용 권장
-                            // 웹 공유 예시 코드
-                            val sharerUrl = WebSharerClient.instance.makeDefaultUrl(defaultFeed)
-
-                            // CustomTabs으로 웹 브라우저 열기
-
-                            // 1. CustomTabsServiceConnection 지원 브라우저 열기
-                            // ex) Chrome, 삼성 인터넷, FireFox, 웨일 등
-                            try {
-                                KakaoCustomTabsClient.openWithDefault(context, sharerUrl)
-                            } catch(e: UnsupportedOperationException) {
-                                // CustomTabsServiceConnection 지원 브라우저가 없을 때 예외처리
-                            }
-
-                            // 2. CustomTabsServiceConnection 미지원 브라우저 열기
-                            // ex) 다음, 네이버 등
-                            try {
-                                KakaoCustomTabsClient.open(context, sharerUrl)
-                            } catch (e: ActivityNotFoundException) {
-                                // 디바이스에 설치된 인터넷 브라우저가 없을 때 예외처리
-                            }
-                        }
+                        //공유
+//                        // 카카오톡 설치여부 확인
+//                        if (ShareClient.instance.isKakaoTalkSharingAvailable(context)) {
+//                            // 카카오톡으로 카카오톡 공유 가능
+//                            ShareClient.instance.shareDefault(context, defaultFeed) { sharingResult, error ->
+//                                if (error != null) {
+//                                    Log.e(TAG, "카카오톡 공유 실패", error)
+//                                }
+//                                else if (sharingResult != null) {
+//                                    Log.d(TAG, "카카오톡 공유 성공 ${sharingResult.intent}")
+//                                    context.startActivity(sharingResult.intent)
+//
+//                                    // 카카오톡 공유에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
+//                                    Log.w(TAG, "Warning Msg: ${sharingResult.warningMsg}")
+//                                    Log.w(TAG, "Argument Msg: ${sharingResult.argumentMsg}")
+//                                }
+//                            }
+//                        } else {
+//                            // 카카오톡 미설치: 웹 공유 사용 권장
+//                            // 웹 공유 예시 코드
+//                            val sharerUrl = WebSharerClient.instance.makeDefaultUrl(defaultFeed)
+//
+//                            // CustomTabs으로 웹 브라우저 열기
+//
+//                            // 1. CustomTabsServiceConnection 지원 브라우저 열기
+//                            // ex) Chrome, 삼성 인터넷, FireFox, 웨일 등
+//                            try {
+//                                KakaoCustomTabsClient.openWithDefault(context, sharerUrl)
+//                            } catch(e: UnsupportedOperationException) {
+//                                // CustomTabsServiceConnection 지원 브라우저가 없을 때 예외처리
+//                            }
+//
+//                            // 2. CustomTabsServiceConnection 미지원 브라우저 열기
+//                            // ex) 다음, 네이버 등
+//                            try {
+//                                KakaoCustomTabsClient.open(context, sharerUrl)
+//                            } catch (e: ActivityNotFoundException) {
+//                                // 디바이스에 설치된 인터넷 브라우저가 없을 때 예외처리
+//                            }
+//                        }
                         }
 
 
@@ -137,26 +138,26 @@ class MyReviewRVAdapter(private val myReviewList: ArrayList<MyReviewResult>): Re
 //            })
 
 
-            defaultFeed = FeedTemplate(
-                content = Content(
-                    title = myreview.eventName,
-                    description = myreview.review,
-                    imageUrl = myreview.pic1,
-                    link = Link(
-                        webUrl = "https://developers.kakao.com",
-                        mobileWebUrl = "https://developers.kakao.com"
-                    )
-                ),
-                buttons = listOf(
-                    Button(
-                        "앱으로 보기",
-                        Link(
-                            androidExecutionParams = mapOf("key1" to "value1", "key2" to "value2"),
-                            iosExecutionParams = mapOf("key1" to "value1", "key2" to "value2")
-                        )
-                    )
-                )
-            )
+//            defaultFeed = FeedTemplate(
+//                content = Content(
+//                    title = myreview.eventName,
+//                    description = myreview.review,
+//                    imageUrl = myreview.pic1,
+//                    link = Link(
+//                        webUrl = "https://developers.kakao.com",
+//                        mobileWebUrl = "https://developers.kakao.com"
+//                    )
+//                ),
+//                buttons = listOf(
+//                    Button(
+//                        "앱으로 보기",
+//                        Link(
+//                            androidExecutionParams = mapOf("key1" to "value1", "key2" to "value2"),
+//                            iosExecutionParams = mapOf("key1" to "value1", "key2" to "value2")
+//                        )
+//                    )
+//                )
+//            )
 
 
         }
