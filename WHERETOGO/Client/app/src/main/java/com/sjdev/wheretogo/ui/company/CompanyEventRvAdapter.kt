865957b21f30a752631d6c507beb540f7c6ad993
hyeon-b <20201009@sungshin.ac.kr>
2023-09-24 16:27:43 +0900

    company visited&saved 버튼 미작동 문제 해결

--- WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/ui/company/CompanyEventRvAdapter.kt
+++ WHERETOGO/Client/app/src/main/java/com/sjdev/wheretogo/ui/company/CompanyEventRvAdapter.kt
@@ -23,6 +23,7 @@ import com.sjdev.wheretogo.data.remote.search.EventResult
 import com.sjdev.wheretogo.data.remote.search.SearchService
 import com.sjdev.wheretogo.databinding.ItemRecycleEventBinding
 import com.sjdev.wheretogo.ui.login.LoginActivity
+import com.sjdev.wheretogo.ui.search.SearchEventAdapter
 import com.sjdev.wheretogo.util.ApplicationClass
 import com.sjdev.wheretogo.util.getJwt
 import retrofit2.Call
@@ -42,7 +43,6 @@ class CompanyEventRvAdapter(var events: ArrayList<CompanyEventResult>, var con: 
 
     var isSavedBtnSelected: Boolean = false
     var isVisitedBtnSelected: Boolean = false
-    val userIdx = getIdx()
 
     fun setOnItemClickListener(itemClickListener: OnItemClickListener) {
         listener = itemClickListener
@@ -52,114 +52,93 @@ class CompanyEventRvAdapter(var events: ArrayList<CompanyEventResult>, var con: 
         val con = parent.context
         val binding: ItemRecycleEventBinding =
             ItemRecycleEventBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        val inflater = con.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val view = inflater.inflate(R.layout.item_recycle_event, parent, false)
 
-        return ViewHolder(view)
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val event = events[position]
+    override fun onBindViewHolder(holder: CompanyEventRvAdapter.ViewHolder, position: Int) {
+        val event: CompanyEventResult = events[position]
 
-        holder.background.setOnClickListener {
-            listener.onItemClick(events[position])
+        holder.binding.itemMypageLikeFrame.setOnClickListener {
+            listener.onItemClick(event)
         }
 
-        holder.eventName.text = event.eventName
-        holder.date.text = String.format(
-            "%s ~ %s",
-            event.startDate.slice(IntRange(0, 9)),
-            event.endDate.slice(IntRange(0, 9))
-        )
-        holder.kind.text = event.kind
+        holder.bind(event, holder)
 
-        if (event.pic != null) {
-            Glide.with(con).load(event.pic)
-                .transform(CenterCrop(), RoundedCorners(40))
-                .into(holder.eventImage)
-        } else {
-            holder.eventImage.setImageResource(R.drawable.default_event_img)
-            holder.eventImage.clipToOutline = true
+    }
+
+
+    inner class ViewHolder(val binding: ItemRecycleEventBinding) :
+        RecyclerView.ViewHolder(binding.root){
+        fun bind(event: CompanyEventResult, holder: CompanyEventRvAdapter.ViewHolder){
+            getEventStatus(event.eventID, holder)
+            binding.itemSearchTitleTv.text = event.eventName
+            binding.itemSearchDateTv.text = String.format(
+                "%s ~ %s",
+                event.startDate.slice(IntRange(0, 9)),
+                event.endDate.slice(IntRange(0, 9))
+            )
+            binding.itemSearchKindTv.text = event.kind
+
+            if (event.pic != null) {
+                Glide.with(con).load(event.pic)
+                    .transform(CenterCrop(), RoundedCorners(56))
+                    .into(binding.itemSearchIv)
+            } else {
+                binding.itemSearchIv.setImageResource(R.drawable.default_event_img)
+                binding.itemSearchIv.clipToOutline = true
+            }
+
+            initClickListener(binding,event,holder)
+
         }
+    }
 
-        getEventStatus(event.eventID, holder)
+    private fun initClickListener(
+        binding: ItemRecycleEventBinding,
+        event: CompanyEventResult,
+        holder: CompanyEventRvAdapter.ViewHolder){
 
         /** visited Button OnClickListener **/
-        holder.visitedBtn.setOnClickListener {
-            if (getJwt() ==null) showLoginAlert()
+        binding.itemSearchVisitedBtn.setOnClickListener {
+            if (getJwt()==null) showLoginAlert()
             else {
                 if (!isVisitedBtnSelected) {
-                    Toast.makeText(con, R.string.visited_on, Toast.LENGTH_SHORT).show()
-                    visitEvent(holder, event.eventID)
+                    Toast.makeText(con,
+                        R.string.visited_on, Toast.LENGTH_SHORT).show()
+                    binding.itemSearchVisitedBtn.setImageResource(R.drawable.btn_check_click)
+                    isVisitedBtnSelected = true
+                    searchService.setVisitedEvent(event.eventID)
                 }
                 else {
                     Toast.makeText(con, R.string.visited_off, Toast.LENGTH_SHORT).show()
-                    holder.visitedBtn.setBackgroundResource(R.drawable.btn_check_unclick)
+                    binding.itemSearchVisitedBtn.setImageResource(R.drawable.btn_check_unclick)
                     isVisitedBtnSelected = false
                     searchService.setDeleteVisitedEvent(event.eventID)
                 }
             }
 
-
         }
 
         /** like Button OnClickListener **/
-        holder.likedBtn.setOnClickListener {
-            if (getJwt() ==null) showLoginAlert()
+        binding.itemSearchLikeBtn.setOnClickListener {
+            if (getJwt()==null) showLoginAlert()
             else {
-                println(isSavedBtnSelected)
                 if (!isSavedBtnSelected) {
                     Toast.makeText(con, R.string.like_on, Toast.LENGTH_SHORT).show()
-                    holder.likedBtn.setBackgroundResource(R.drawable.btn_like_click)
+                    binding.itemSearchLikeBtn.setImageResource(R.drawable.btn_like_click)
                     isSavedBtnSelected = true
                     searchService.setSavedEvent(event.eventID)
-
                 }
                 else {
                     Toast.makeText(con, R.string.like_off, Toast.LENGTH_SHORT).show()
-                    holder.likedBtn.setBackgroundResource(R.drawable.btn_like_unclick)
+                    binding.itemSearchLikeBtn.setImageResource(R.drawable.btn_like_unclick)
                     isSavedBtnSelected = false
                     searchService.setDeleteSavedEvent(event.eventID)
                 }
             }
 
         }
-    }
-
-
-    inner class ViewHolder(itemView: View) :
-        RecyclerView.ViewHolder(itemView) {
-        var eventName: TextView
-        var date: TextView
-        var kind: TextView
-
-        var visitedBtn: ImageButton
-        var likedBtn: ImageButton
-
-        var background: View
-        var eventImage: ImageView
-
-        init {
-            eventName = itemView.findViewById(R.id.item_search_title_tv)
-            date = itemView.findViewById(R.id.item_search_date_tv)
-            background = itemView.findViewById(R.id.item_search_background)
-            eventImage = itemView.findViewById(R.id.item_search_iv)
-            kind = itemView.findViewById(R.id.item_search_kind_tv)
-
-            visitedBtn = itemView.findViewById(R.id.item_search_visited_btn)
-            likedBtn = itemView.findViewById(R.id.item_search_like_btn)
-
-
-        }
-    }
-
-    private fun visitEvent(holder: ViewHolder, eventId: Int){
-        Toast.makeText(con,
-            R.string.visited_on, Toast.LENGTH_SHORT).show()
-        holder.visitedBtn.setBackgroundResource(R.drawable.btn_check_click)
-        isVisitedBtnSelected = true
-
-        searchService.setVisitedEvent(eventId)
     }
 
     private fun getEventStatus(eventId: Int, holder: CompanyEventRvAdapter.ViewHolder) {
@@ -172,18 +151,18 @@ class CompanyEventRvAdapter(var events: ArrayList<CompanyEventResult>, var con: 
                 when (resp.code) {
                     1000 -> {
                         if (resp.result.isVisited) {
-                            holder.visitedBtn.setBackgroundResource(R.drawable.btn_check_click)
+                            holder.binding.itemSearchVisitedBtn.setImageResource(R.drawable.btn_check_click)
                             isVisitedBtnSelected = true
                         } else {
-                            holder.visitedBtn.setBackgroundResource(R.drawable.btn_check_unclick)
+                            holder.binding.itemSearchVisitedBtn.setImageResource(R.drawable.btn_check_unclick)
                             isVisitedBtnSelected = false
                         }
 
                         if (resp.result.isSaved) {
-                            holder.likedBtn.setBackgroundResource(R.drawable.btn_like_click)
+                            holder.binding.itemSearchLikeBtn.setImageResource(R.drawable.btn_like_click)
                             isSavedBtnSelected = true
                         } else {
-                            holder.likedBtn.setBackgroundResource(R.drawable.btn_like_unclick)
+                            holder.binding.itemSearchLikeBtn.setImageResource(R.drawable.btn_like_unclick)
                             isSavedBtnSelected = false
                         }
                     }
@@ -207,10 +186,6 @@ class CompanyEventRvAdapter(var events: ArrayList<CompanyEventResult>, var con: 
     }
 
 
-    fun setMyEvent(result: Boolean): Boolean {
-        return result
-    }
-
     private fun showLoginAlert() {
         AlertDialog.Builder(con)
             .setMessage("해당 기능을 사용하려면 로그인이 필요합니다.\n로그인 페이지로 이동하시겠습니까?")
@@ -221,11 +196,6 @@ class CompanyEventRvAdapter(var events: ArrayList<CompanyEventResult>, var con: 
             .setNegativeButton("아니오") { _, _ ->
             }
             .show()
-    }
-
-    private fun getIdx(): Int {
-        val spf = con.getSharedPreferences("userInfo", AppCompatActivity.MODE_PRIVATE)
-        return spf!!.getInt("userIdx", -1)
     }
 
 }
